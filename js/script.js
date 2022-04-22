3c5079462c6597884be506ecf57c40f03163fcd1
KIO.network <92652974+Dimagio03@users.noreply.github.com>
2022-04-22 05:22:14 +0000

    immortal8

--- js/script.js
+++ js/script.js
@@ -2,30 +2,92 @@
 		
 	//Page cursors
 
-    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
-        t.style.left = n.clientX + "px", 
-		t.style.top = n.clientY + "px", 
-		e.style.left = n.clientX + "px", 
-		e.style.top = n.clientY + "px", 
-		i.style.left = n.clientX + "px", 
-		i.style.top = n.clientY + "px"
-    });
-    var t = document.getElementById("cursor"),
-        e = document.getElementById("cursor2"),
-        i = document.getElementById("cursor3");
-    function n(t) {
-        e.classList.add("hover"), i.classList.add("hover")
-    }
-    function s(t) {
-        e.classList.remove("hover"), i.classList.remove("hover")
-    }
-    s();
-    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
-        o(r[a])
-    }
-    function o(t) {
-        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
-    }
+ //Page cursors
+	
+ $(window).mousemove(function(e) { 	  
+	$(".cursor").css({
+		left: e.pageX,
+		top: e.pageY
+	})	  
+})
+$(".cursor-link")
+.on("mouseenter", function() {	 
+$('.cursor').addClass("active")	  
+})
+.on("mouseleave", function() {	  
+$('.cursor').removeClass("active")	  
+})		
+//Blog page cursors with image	
+//Blog post 1:
+$(".cursor-link-blog-post-1")
+.on("mouseenter", function() {	 
+$('.cursor').addClass("active-blog-post-1")	  
+})
+.on("mouseleave", function() {	  
+$('.cursor').removeClass("active-blog-post-1")	  
+})	
+//Blog post 2:
+$(".cursor-link-blog-post-2")
+.on("mouseenter", function() {	 
+$('.cursor').addClass("active-blog-post-2")	  
+})
+.on("mouseleave", function() {	  
+$('.cursor').removeClass("active-blog-post-2")	  
+})	
+//Blog post 3:
+$(".cursor-link-blog-post-3")
+.on("mouseenter", function() {	 
+$('.cursor').addClass("active-blog-post-3")	  
+})
+.on("mouseleave", function() {	  
+$('.cursor').removeClass("active-blog-post-3")	  
+})		
+//Blog post 4:
+$(".cursor-link-blog-post-4")
+.on("mouseenter", function() {	 
+$('.cursor').addClass("active-blog-post-4")	  
+})
+.on("mouseleave", function() {	  
+$('.cursor').removeClass("active-blog-post-4")	  
+})	
+//Blog post 5:
+$(".cursor-link-blog-post-5")
+.on("mouseenter", function() {	 
+$('.cursor').addClass("active-blog-post-5")	  
+})
+.on("mouseleave", function() {	  
+$('.cursor').removeClass("active-blog-post-5")	  
+})		
+//Blog post 6:
+$(".cursor-link-blog-post-6")
+.on("mouseenter", function() {	 
+$('.cursor').addClass("active-blog-post-6")	  
+})
+.on("mouseleave", function() {	  
+$('.cursor').removeClass("active-blog-post-6")	  
+})		
+//Blog post 7:
+$(".cursor-link-blog-post-7")
+.on("mouseenter", function() {	 
+$('.cursor').addClass("active-blog-post-7")	  
+})
+.on("mouseleave", function() {	  
+$('.cursor').removeClass("active-blog-post-7")	  
+})
+
+
+//Parallax & fade on scroll	
+
+function scrollBanner() {
+  $(document).on('scroll', function(){
+	var scrollPos = $(this).scrollTop();
+	$('.parallax-fade-top').css({
+	  'top' : (scrollPos/2)+'px',
+	  'opacity' : 1-(scrollPos/240)
+	});
+  });    
+}
+scrollBanner();	
 	
 	//Navigation
 
